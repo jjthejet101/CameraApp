2cba22888941aae5438b3ddd91545ab9a937237f
Unknown <justinkjetton@gmail.com>
2018-06-19 14:36:36 -0500

    geolocation attempt

--- CameraApp/www/js/index.js
+++ CameraApp/www/js/index.js
@@ -9,9 +9,20 @@ let app = {
       sourceType: Camera.PictureSourceType.CAMERA,
       mediaType: Camera.MediaType.PICTURE,
       encodingType: Camera.EncodingType.JPEG,
-      cameraDirection: Camera.Direction.FRONT,
+      cameraDirection: Camera.Direction.FRONT
+    }
+    var onSuccess = function (position) {
+      alert('Latitude: '          + position.coords.latitude          + '\n' +
+            'Longitude: '         + position.coords.longitude         + '\n' +
+            'Altitude: '          + position.coords.altitude          + '\n' +
+            'Accuracy: '          + position.coords.accuracy          + '\n' +
+            'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+            'Heading: '           + position.coords.heading           + '\n' +
+            'Speed: '             + position.coords.speed             + '\n' +
+            'Timestamp: '         + position.timestamp                + '\n');
     }
     navigator.camera.getPicture(app.ftw, app.wtf, opts)
+    navigator.geolocation.getCurrentPosition(onSuccess);
   },
   ftw: function (imageData) {
     document.getElementById('msg').textContent = imageData
@@ -19,6 +30,10 @@ let app = {
   },
   wtf: function (msg) {
     document.getElementById('msg').textContent = msg
+    function onError(error) {
+      alert('code: '    + error.code    + '\n' +
+            'message: ' + error.message + '\n');
+    }
   }
 }
 
