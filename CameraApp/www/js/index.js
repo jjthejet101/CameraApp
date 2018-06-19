f695700ea3bb2e4a11f8befed86145765f4e39ef
Unknown <justinkjetton@gmail.com>
2018-06-19 00:11:53 -0500

    ready to test?

--- CameraApp/www/js/index.js
+++ CameraApp/www/js/index.js
@@ -1,15 +1,27 @@
 let app = {
   init: function () {
-
+    document.getElementById('btn').addEventListener('click', app.takephoto)
   },
   takephoto: function () {
-
+    let opts = {
+      quality: 80,
+      destinationType: Camera.DestinationType.FILE_URI,
+      sourceType: Camera.PictureSourceType.CAMERA,
+      mediaType: Camera.MediaType.PICTURE,
+      encodingType: Camera.EncodingType.JPEG,
+      cameraDirection: Camera.Direction.FRONT,
+      targetWidth: 300,
+      targetHeight: 400
+    }
+    navigator.camera.getPicture(app.ftw, app.wtf, opts)
   },
-  ftw: function () {
-
+  ftw: function (imgURI) {
+    document.getElementById('msg').textContent = imgURI
+    document.getElementById('photo').src = imgURI
   },
-  wtf: function () {
-
+  wtf: function (msg) {
+    document.getElementById('msg').textContent = msg
   }
 }
+
 document.addEventListener('deviceready', app.init)
